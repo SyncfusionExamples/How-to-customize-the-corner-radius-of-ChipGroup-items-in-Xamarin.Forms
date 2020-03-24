de3fe57d52eefb1262e6c40082f312f5127db4c9
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-03-24 17:30:37 +0530

    Demo sample updated

---
+++ SfChipGroupCornerRadius/SfChipGroupCornerRadius/CustomChipGroup.cs
@@ -0,0 +1,48 @@
+﻿using Syncfusion.XForms.Buttons;
+using Xamarin.Forms;
+
+namespace SfChipGroupCornerRadius
+{
+    public class CustomChipGroup : SfChipGroup
+    {
+        public static readonly BindableProperty CornerRadiusProperty =
+            BindableProperty.Create("CornerRadius", typeof(Thickness), typeof(CustomChipGroup), GetDefaultCornerRadius(), BindingMode.TwoWay, null, null);
+
+        public Thickness CornerRadius
+        {
+            get { return (Thickness)GetValue(CornerRadiusProperty); }
+            set { this.SetValue(CornerRadiusProperty, value); }
+        }
+
+        protected override void OnChildAdded(Element child)
+        {
+            base.OnChildAdded(child);
+            child.ChildAdded += Child_ChildAdded;
+        }
+
+        private void Child_ChildAdded(object sender, ElementEventArgs e)
+        {
+            var sfChip = e.Element as SfChip;
+            if (sfChip != null)
+            {
+                sfChip.SetBinding(SfChip.CornerRadiusProperty, new Binding() { Path = "CornerRadius", Source = this, Mode = BindingMode.TwoWay });
+            }
+        }
+
+        protected override void OnChildRemoved(Element child)
+        {
+            child.ChildAdded -= Child_ChildAdded;
+            base.OnChildRemoved(child);
+        }
+
+        private static Thickness GetDefaultCornerRadius()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return 40d;
+            }
+
+            return 15d;
+        }
+    }
+}
